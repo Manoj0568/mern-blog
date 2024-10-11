527c20126a7cd36626e4fc596933ce6ff50c5529
Manoj Manoharan <manojmanoharan255@gmail.com>
2024-10-11 09:32:45 +0530

    Routes and pages were added in the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
-
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Projects from "./pages/Projects"
 
 function App() {
  
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/about" element={<About/>}/>
+       <Route path="/signup" element={<Signup/>}/>
+       <Route path='/signin' element={<Signin/>}/>
+       <Route path='/projects' element={<Projects/>}/>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
